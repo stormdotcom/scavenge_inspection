1a03b4c05d5e292b6bab606784971b8a37eb29e6
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2023-07-25 17:34:10 +0530

    added updated message

--- src/modules/profile/saga.js
+++ src/modules/profile/saga.js
@@ -1,14 +1,19 @@
-import { all, call, takeLatest } from "redux-saga/effects";
+import { all, call, fork, put, take, takeLatest } from "redux-saga/effects";
 import { ACTION_TYPES } from "./actions";
 import { updateVesselInfoApi, fetchVesselInfoApi } from "./api";
 import { handleAPIRequest } from "../../utils/http";
+import { successNotify } from "../../utils/notificationUtils";
 
 export function* updateVesselInfo({ payload }) {
     yield call(handleAPIRequest, updateVesselInfoApi, payload);
 }
 
 export function* fetchVesselInfo() {
-    yield call(handleAPIRequest, fetchVesselInfoApi);
+    yield fork(handleAPIRequest, fetchVesselInfoApi);
+    const responseAction = yield take([ACTION_TYPES.UPDATE_VESSEL_SUCCESS, ACTION_TYPES.UPDATE_VESSEL_FAILURE]);
+    if (responseAction.type === ACTION_TYPES.UPDATE_VESSEL_SUCCESS) {
+        yield put(successNotify({ title: "Success", message: "Vessel Details Updated" }));
+    }
 }
 
 
