eb847702a70be303592f7470a8da93af62fda308
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2023-08-31 18:15:47 +0530

    added bottom card for management

--- src/modules/admin/components/AdminDashboard.js
+++ src/modules/admin/components/AdminDashboard.js
@@ -11,8 +11,13 @@ import { STATE_REDUCER_KEY } from "../constants";
 import { fetchDashboardCards } from "../actions";
 import { formatNumber } from "./utils";
 import LoadingCustomOverlay from "../../common/components/LoadingOverlay";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import RoomPreferencesIcon from "@mui/icons-material/RoomPreferences";
+import WavesIcon from "@mui/icons-material/Waves";
+import { useNavigate } from "react-router-dom";
 
 const AdminDashboard = () => {
+    const navigate = useNavigate();
     const { data: { organizations = 0, totalUsers = 0, fleetManagers = 0, vessels = 0, reports = 0, cylinderImageCount = 0 } = {}, requestInProgress = false } =
         useSelector(state => state[STATE_REDUCER_KEY].dashboardCards);
     const dispatch = useDispatch();
@@ -76,6 +81,37 @@ const AdminDashboard = () => {
                         </Box>
                     </Paper>
                 </LoadingCustomOverlay>
+            </Grid>
+        </Grid>
+        <Grid container rowSpacing={2} columnSpacing={3}>
+            <Grid item sm={12} md={6} lg={6} xl={6}>
+                <Paper sx={{ height: "140x", width: "100%", bgcolor: "primary.light", mt: 1, p: 3 }}>
+                    <Box sx={{ height: "200px" }}>
+
+                    </Box>
+                </Paper>
+            </Grid>
+            <Grid item sm={12} md={6} lg={6} xl={6}>
+                <Paper sx={{ height: "140x", width: "100%", bgcolor: "primary.light", mt: 1, p: 3 }}>
+                    <Grid container>
+                        <Grid item sm={12} md={6} lg={6} xl={6}>
+                            <Box sx={{ height: "140x", width: "100%", display: "flex", bgcolor: "primary.light", flexDirection: "column", mt: 1, p: 3, justifyContent: "center" }}>
+                                <Box onClick={() => navigate("../users")} sx={{ display: "flex", justifyContent: "flex-start", my: 1, "&:hover": { bgcolor: "primary.dark", cursor: "pointer" }, width: "100%" }} >
+                                    <Box> <ManageAccountsIcon /></Box>
+                                    <Typography sx={{ fontWeight: 600, pl: 1 }}> Manage Users</Typography>
+                                </Box>
+                                <Box onClick={() => navigate("../organizations")} sx={{ display: "flex", justifyContent: "flex-start", my: 1, "&:hover": { bgcolor: "primary.dark", cursor: "pointer" }, width: "100%" }}>
+                                    <Box>             <RoomPreferencesIcon /></Box>
+                                    <Typography sx={{ fontWeight: 600, pl: 1 }}> Organizations</Typography>
+                                </Box>
+                                <Box onClick={() => navigate("../vessels")} sx={{ display: "flex", justifyContent: "flex-start", my: 1, "&:hover": { bgcolor: "primary.dark", cursor: "pointer" }, width: "100%" }}>
+                                    <Box> <WavesIcon /></Box>
+                                    <Typography sx={{ fontWeight: 600, pl: 1 }}> Vessels</Typography>
+                                </Box>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Paper>
             </Grid>
         </Grid>
     </Box >;
