5cbc904b93ce6d6bcdb369dcc5c0fd00a0b47ca6
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2023-09-01 00:08:37 +0530

    Overlay added for non approved user

--- src/modules/vessel/Home.js
+++ src/modules/vessel/Home.js
@@ -4,7 +4,9 @@ function UserHome() {
 
     return (
         <div>
+            sdfsdf
             <Outlet />
+            sdfsdf
         </div>
     );
 }
--- src/modules/vessel/components/HomeWrapper.js
+++ src/modules/vessel/components/HomeWrapper.js
@@ -2,9 +2,14 @@ import React from "react";
 import VesselInspectionDetails from "./VesselInspectionDetails";
 import ImageUploader from "./Report/ImageUploader";
 import ReportTable from "./ReportTable";
+import Overlay from "./OverLay";
+import { useSelector } from "react-redux";
+import { STATE_REDUCER_KEY as COMMON } from "../../common";
 
 const HomeWrapper = () => {
+    const approvedStatus = useSelector(state => state[COMMON].user?.approvedStatus) || false;
     return <>
+        <Overlay active={approvedStatus} />
         <VesselInspectionDetails />
         <ImageUploader />
         <ReportTable />
