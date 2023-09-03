4a5f6ea0f0080175664d03c0e9e4a843ebdc1c03
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2023-09-03 12:55:47 +0530

    styles done

--- src/modules/admin/components/AdminDashboard.js
+++ src/modules/admin/components/AdminDashboard.js
@@ -13,7 +13,6 @@ import { formatNumber } from "./utils";
 import LoadingCustomOverlay from "../../common/components/LoadingOverlay";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 import RoomPreferencesIcon from "@mui/icons-material/RoomPreferences";
-import WavesIcon from "@mui/icons-material/Waves";
 import { useNavigate } from "react-router-dom";
 import SquareStatsCad from "./SquareStatsCad";
 
@@ -110,21 +109,47 @@ const AdminDashboard = () => {
             </Grid>
             <Grid item sm={12} md={6} lg={5} xl={5} p={1}>
                 <Typography sx={{ fontWeight: 700, fontSize: "16px", color: "secondary.dark" }}>{"Other Stats"}</Typography>
-                <Paper sx={{ height: "140x", width: "100%", bgcolor: "primary.light", mt: 1, p: 3 }}>
-                    <Grid container>
-                        <Grid item sm={12} md={6} lg={6} xl={6}>
-                            <Box sx={{ height: "140x", width: "100%", display: "flex", bgcolor: "primary.light", flexDirection: "column", mt: 1, p: 3, justifyContent: "center" }}>
-                                <Box onClick={() => navigate("../users")} sx={{ display: "flex", justifyContent: "flex-start", my: 1, "&:hover": { bgcolor: "primary.dark", cursor: "pointer" }, width: "100%" }} >
-                                    <Box> <ManageAccountsIcon /></Box>
-                                    <Typography sx={{ fontWeight: 600, pl: 1 }}> Manage Users</Typography>
+                <Box sx={{ height: "140x", width: "100%", mt: 1, p: 3, pl: 0, display: "flex" }}>
+                    <Grid container rowSpacing={1}>
+                        <Grid item xs={12} sm={4}>
+                            <SquareStatsCad value={111} type="Total Revenue" cardType="dark" />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <SquareStatsCad value={111} type="Total Transaction" cardType="dark" />
+                        </Grid>
+                    </Grid>
+                </Box>
+
+            </Grid>
+        </Grid>
+        <Grid container rowSpacing={2} columnSpacing={3}>
+            <Grid item sm={12} md={12} lg={12} xl={12} p={1}>
+                <Typography sx={{ fontWeight: 700, fontSize: "16px", color: "secondary.dark" }}>{"User Management"}</Typography>
+                <Paper sx={{ height: "140x", width: "100%", bgcolor: "primary.light", mt: 1, p: 3, display: "flex", justifyContent: "center" }}>
+                    <Grid container columnSpacing={4}>
+                        <Grid item sm={6} md={4} lg={4} xl={4} >
+                            <Box onClick={() => navigate("../users")} sx={{ height: "140x", width: "100%", borderRadius: "15px", display: "flex", bgcolor: "primary.300", flexDirection: "column", mt: 1, p: 3, justifyContent: "center", "&:hover": { bgcolor: "primary.dark", cursor: "pointer" } }}>
+                                <Box sx={{ display: "flex", justifyContent: "flex-start", my: 1, width: "100%" }} >
+                                    <Box> <ManageAccountsIcon sx={{ color: "secondary.light" }} /></Box>
+                                    <Typography sx={{ fontWeight: 600, pl: 1, color: "secondary.light" }}> Manage Users</Typography>
                                 </Box>
-                                <Box onClick={() => navigate("../organizations")} sx={{ display: "flex", justifyContent: "flex-start", my: 1, "&:hover": { bgcolor: "primary.dark", cursor: "pointer" }, width: "100%" }}>
-                                    <Box>             <RoomPreferencesIcon /></Box>
-                                    <Typography sx={{ fontWeight: 600, pl: 1 }}> Organizations</Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item sm={6} md={4} lg={4} xl={4}>
+                            <Box onClick={() => navigate("../organizations")} sx={{ height: "140x", width: "100%", borderRadius: "15px", display: "flex", bgcolor: "primary.300", flexDirection: "column", mt: 1, p: 3, justifyContent: "center", "&:hover": { bgcolor: "primary.dark", cursor: "pointer" } }}>
+                                <Box sx={{ display: "flex", justifyContent: "flex-start", my: 1, width: "100%" }}>
+                                    <Box>             <RoomPreferencesIcon sx={{ color: "secondary.light" }} /></Box>
+                                    <Typography sx={{ fontWeight: 600, pl: 1, color: "secondary.light" }}> Organizations</Typography>
                                 </Box>
-                                <Box onClick={() => navigate("../vessels")} sx={{ display: "flex", justifyContent: "flex-start", my: 1, "&:hover": { bgcolor: "primary.dark", cursor: "pointer" }, width: "100%" }}>
-                                    <Box> <WavesIcon /></Box>
-                                    <Typography sx={{ fontWeight: 600, pl: 1 }}> Vessels</Typography>
+
+                            </Box>
+                        </Grid>
+                        <Grid item sm={6} md={4} lg={4} xl={4}>
+                            <Box onClick={() => navigate("../vessels")} sx={{ height: "140x", width: "100%", borderRadius: "15px", display: "flex", bgcolor: "primary.300", flexDirection: "column", mt: 1, p: 3, justifyContent: "center", "&:hover": { bgcolor: "primary.dark", cursor: "pointer" } }}>
+
+                                <Box sx={{ display: "flex", justifyContent: "flex-start", my: 1, width: "100%" }}>
+                                    <Box> <DirectionsBoatIcon sx={{ color: "secondary.light" }} /></Box>
+                                    <Typography sx={{ fontWeight: 600, pl: 1, color: "secondary.light" }}> Vessels</Typography>
                                 </Box>
                             </Box>
                         </Grid>
