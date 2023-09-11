88e81b6c59ed6e7ebfbd4f2e248f3de7d94b46c8
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2023-09-11 15:55:02 +0530

    ui changes in signup

--- src/modules/organization/components/CircleYellow.js
+++ src/modules/organization/components/CircleYellow.js
@@ -8,7 +8,7 @@ const DateCircle = ({ latestInspectionDate = "" }) => {
         <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", p: 0.8, position: "absolute", top: "-10px", left: "10px" }}>
             <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", borderRadius: "50%", bgcolor: "#31313166", border: "1px solid black", width: "50px", height: "50px" }}>
                 <Typography sx={{ fontSize: "9px", color: "secondary.light" }}> {"Latest"} </Typography>
-                <Typography sx={{ fontWeight: 600, fontSize: "9px", color: "secondary.light" }}> {fromEpochToMuiDate(latestInspectionDate)}</Typography>
+                <Typography sx={{ fontWeight: { xs: 400, md: 600 }, fontSize: "9px", color: "secondary.light" }}> {fromEpochToMuiDate(latestInspectionDate)}</Typography>
             </Box>
         </Box>
     </Box>;
@@ -21,7 +21,7 @@ const CircleYellow = ({ latestInspectionDate = "", inspectionCount = 0 }) => {
         <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "50%", bgcolor: "secondary.extraLight", width: "65px", height: "65px" }}>
             <Typography sx={{ fontWeight: 800, fontSi: "16px" }}> {inspectionCount}</Typography>
         </Box>
-        <Box> <Typography sx={{ color: "white.main", display: "inline", fontWeight: 500 }}>{"Total Inspections"}</Typography></Box>
+        <Box> <Typography sx={{ color: "white.main", display: "inline", fontWeight: 500, fontSize: { xs: "11px", md: "14px" } }}>{"Total Inspections"}</Typography></Box>
     </Box>;
 };
 
