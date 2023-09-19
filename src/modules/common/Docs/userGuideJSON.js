3130a6bb4ad12f08ea6f5c9a127822e8aee7d322
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2023-09-19 12:36:50 +0530

    added remarks

---
+++ src/modules/common/Docs/userGuideJSON.js
@@ -0,0 +1,31 @@
+const userGuide = {
+    sections: [
+        {
+            title: "New Organization",
+            image: "",
+            content: "When a fleet manager from a new organization is signing up, they should select 'New Organization' the checkbox at the top of the screen. And in the organization name input box he should type the organization name."
+        },
+        {
+            title: "Existing Organization",
+            image: "",
+            content: "When fleet managers from existing organizations sign up, they need to select 'Existing Organization' from the checkbox. And in the Organization name a dropdown will appear where they need to select the organization name."
+        },
+        {
+            title: "New Vessel",
+            image: "",
+            content: "When personnel is signing up as a vessel for the first time, they will need to select the organization they are from and the manager under whom the vessel is in."
+        },
+        {
+            title: "Functionalities of Vessel",
+            image: "",
+            content: "From a vessel login, you can have a list of features that you can do, from uploading images for inspection to view older reports. A Vessel will be having the options to have all the details about the inspection date and will also be able to edit other things like total running hours, cylinder oil type, cylinder oil consumption, running hours while uploading each inspection report."
+        },
+        {
+            title: "Upload Cylinder Image",
+            image: "",
+            content: "And when you click on the 'Upload Cylinder Image' Button it’ll open a popup where you can specify the cylinder number and upload the cylinder image for prediction. This is how your predictions will show up on the screen whenever you upload a cylinder image. And for each cylinder, you can type remarks according to the status of the cylinder before saving or downloading the report."
+        }
+    ]
+};
+
+export { userGuide };
